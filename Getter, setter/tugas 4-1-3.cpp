e2b1d52c6e323a0c55c99c4f64dd23824a2f8a59
Utza Reginald <153581938+UtzaReginald@users.noreply.github.com>
2024-04-02 11:19:14 +0700

    Add files via upload

---
+++ Getter, setter/tugas 4-1-3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Mahasiswa{
+	public:
+		Mahasiswa(string nama, int umur)
+		: nama_(nama), umur_(umur){
+			cout<<"Constructor Mahasiswa"<<endl;
+		}
+		~Mahasiswa(){
+			cout<<"==Contoh Destructor Mahasiswa=="<<endl;
+		}
+		void tempilkanbiodata(){
+			cout<<"Nama : "<<nama_<<endl;
+			cout<<"Umur : "<<umur_<<endl;
+		}
+		private:
+			string nama_;
+			int umur_;
+};
+
+int main(){
+	{
+		Mahasiswa mhs("Katarina",20);
+		mhs.tempilkanbiodata();
+	}
+	return 0;
+}
---
+++ Getter, setter/tugas 4-2.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+
+class info{
+	private:
+		string nama[100];
+		int nomor[100];
+	
+	public:
+		info(){
+			nama[0] = "Tidak Diketahui";
+			nomor[0] = 0;			
+		}
+		
+		void data(int x[], string a[], int c){
+			for(int i = 0; i < c; i++) {
+				nama[i] = a[i];
+				nomor[i] = x[i];
+			}
+		}
+		
+		void print(int j){
+			for(int i = 1; i < j; i++){
+				cout<<"\nKontak ke-"<<i<<endl
+					<<"Nama: "<<nama[i]<<endl
+					<<"Nomor Telpon: "<<nomor[i]<<endl;
+			}
+		}
+};
+
+int main(){
+	int c=1, z, d[100];
+	string a, b[1000];
+	char x;
+	info inf;
+	cout<<"Masukan Kontak";
+	
+	do{
+		cout<<"\nMasukan Nama Kontak ke-"<<c<<": ";
+		getline(cin >> ws, a);
+		cout<<"Masukan Nomor: ";
+		cin >> d[c];
+		
+		b[c] = a;
+		c++;
+		cout<<"Kontak Berhasil Ditambahkan\n";
+		
+		cout<<"Tambahkan Kontak Lagi? (y/n)";
+		cin>>x;
+	}
+	while(x == 'y');
+	
+	inf.data(d, b, c);
+
+	inf.print(c);
+
+	return 0;
+}
+
+
